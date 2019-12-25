ec776ced52ce8d45375ea96da7c24395838951b5
Nick Solonyy <nicksolony@gmail.com>
2019-12-25 20:32:46 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative "./user.rb"
 
+class Student < User
+  
+  
+  def initialize (first_name,last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+  
+  
 end
+
